89050f09f78b784dede9d1bdb0c4adca50a23891
No99x <86026510+No99x@users.noreply.github.com>
2021-08-02 23:31:26 +0530

    Update apks.js

--- plugins/apks.js
+++ plugins/apks.js
@@ -9,34 +9,17 @@ const fs = require('fs');
 if (Config.WORKTYPE == 'private') {
 
    XTroid.addCMD({pattern: 'apkmod', fromMe: true}, (async (message, match) => {
-      await message.sendMessage('You can get 1000+ Mod apps from our mod stor\n*t.me/danuma*');
+      await message.sendMessage('┏━━━━━━━━━━━━━━━━━━━\n┃〘 ☣️ *Mod App World* ☣️ 〙\n┗━━━━━━━━━━━━━━━━━━━\n');
+      await message.sendMessage('\n*t.me/danuma*\n');
    }));
-
-   XTroid.addCMD({pattern: 'fatp', fromMe: false}, (async (message, match) => {        
-        await message.client.sendMessage(
-            message.jid, 
-            fs.readFileSync("media/gif/apk/FATP.mp3"),
-            MessageType.audio, 
-            {mimetype: Mimetype.mp4Audio}
-        )
-   }));
-
 
 }
 
 else if (Config.WORKTYPE == 'public') {
 
    XTroid.addCMD({pattern: 'apkmod', fromMe: false}, (async (message, match) => {
-      await message.sendMessage('You can get 1000+ Mod apps from our mod stor\n*t.me/danuma*');
+      await message.sendMessage('┏━━━━━━━━━━━━━━━━━━━\n┃〘 ☣️ *Mod App World* ☣️ 〙\n┗━━━━━━━━━━━━━━━━━━━\n');
+      await message.sendMessage('\n*t.me/danuma*\n');
    }));
-    
-   XTroid.addCMD({pattern: 'fatp', fromMe: false}, (async (message, match) => {        
-        await message.client.sendMessage(
-            message.jid, 
-            fs.readFileSync("media/gif/apk/FATP.mp3"),
-            MessageType.audio, 
-            {mimetype: Mimetype.mp4Audio}
-        )
-   }));
-
+   
 }
