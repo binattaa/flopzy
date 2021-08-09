1557650d9ab50c5dbbbca189890e6fbe8346144d
chopzzz <88554759+chopzzz@users.noreply.github.com>
2021-08-09 23:54:26 +0530

    Update chopzpack.js

--- plugins/chopzpack.js
+++ plugins/chopzpack.js
@@ -1,27 +1,26 @@
-const LASIapi = require('textmaker-lasi');
+const lasiapi = require('textmaker-lasi');
 const XTroid = require('../events');
+const GG = "...."
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
 const axios = require('axios')
 const request = require('request');
-const os = require('os');
-var desc_msg = ''
-if (Config.LANG == 'TR') desc_msg = 'Sınırsız erişime sahip textmaker araçlarını gösterir.'
-if (Config.LANG == 'EN') desc_msg = 'Shows textmaker tools with unlimited access.'
-if (Config.LANG == 'RU') desc_msg = 'Показывает инструменты для создания текстов с неограниченным доступом.'
-if (Config.LANG == 'AZ') desc_msg = 'Sınırsız girişi olan textmaker alətləri göstərir.'
-if (Config.LANG == 'PT') desc_msg = 'Mostra ferramentas textmaker com acesso ilimitado.'
-if (Config.LANG == 'ID') desc_msg = 'Menampilkan alat pembuat teks dengan akses tak terbatas.'
-if (Config.LANG == 'ML') desc_msg = 'പരിധിയില്ലാത്ത ആക്സസ് ഉള്ള ടെക്സ്റ്റ് മേക്കർ ഉപകരണങ്ങൾ കാണിക്കുന്നു.'
-if (Config.LANG == 'HI') desc_msg = 'असीमित एक्सेस के साथ टेक्स्टमेकर टूल दिखाता है।'
-if (Config.LANG == 'ES') desc_msg = 'Muestra herramientas de creación de textos con acceso ilimitado.'
-let wk = Config.WORKTYPE == 'public' ? false : true
+const need = "*type some word after command*\n*විධානයට පසුව වචනයක් ලියන්න"
+let FM = Config.WORKTYPE == 'public' ? false : true
+    
 
-XTroid.addCMD({pattern: '.nclub ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
+
+XTroid.addCMD({pattern: 'nclub ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+    const lc = lasi
+    const seed = Config.LOCK
+    if (lc !== seed) return await message.sendMessage(Config.GG);
 
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html",
+    lasiapi.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -31,19 +30,27 @@ XTroid.addCMD({pattern: '.nclub ?(.*)', fromMe: wk, dontAddCMDList: true}, (asyn
                   });
               };
 
-              await download(`${data}`, '/root/lizy/nclub.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/nclub.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/neww.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/neww.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+       
+    });});
 }));
 
-XTroid.addCMD({pattern: '.ngreen ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+XTroid.addCMD({pattern: 'ngreen ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/green-neon-text-effect-874.html",
+    lasiapi.textpro("https://textpro.me/green-neon-text-effect-874.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -53,19 +60,26 @@ XTroid.addCMD({pattern: '.ngreen ?(.*)', fromMe: wk, dontAddCMDList: true}, (asy
                   });
               };
 
-              await download(`${data}`, '/root/lizy/ngreen.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/ngreen.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/glite.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/glite.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.nlux ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+XTroid.addCMD({pattern: 'nsteel ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/3d-luxury-gold-text-effect-online-1003.html",
+    lasiapi.textpro("https://textpro.me/eroded-metal-text-effect-834.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -75,19 +89,25 @@ XTroid.addCMD({pattern: '.nlux ?(.*)', fromMe: wk, dontAddCMDList: true}, (async
                   });
               };
 
-              await download(`${data}`, '/root/lizy/nlux.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/nlux.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/ysteel.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/ysteel.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.ngold ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+XTroid.addCMD({pattern: 'nxms ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/hexa-golden-text-effect-842.html",
+    lasiapi.textpro("https://textpro.me/create-a-christmas-holiday-snow-text-effect-1007.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -97,19 +117,25 @@ XTroid.addCMD({pattern: '.ngold ?(.*)', fromMe: wk, dontAddCMDList: true}, (asyn
                   });
               };
 
-              await download(`${data}`, '/root/lizy/ngold.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/ngold.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lxmas.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lxmas.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.nblood ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+XTroid.addCMD({pattern: 'ncloud ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/blood-text-on-the-frosted-glass-941.html",
+    lasiapi.textpro("https://textpro.me/create-a-cloud-text-effect-on-the-sky-online-1004.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -119,19 +145,26 @@ XTroid.addCMD({pattern: '.nblood ?(.*)', fromMe: wk, dontAddCMDList: true}, (asy
                   });
               };
 
-              await download(`${data}`, '/root/lizy/nblood.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/nblood.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lcloud.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lcloud.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.n2bld ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+XTroid.addCMD({pattern: 'nblood ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/horror-blood-text-effect-online-883.html",
+    lasiapi.textpro("https://textpro.me/blood-text-on-the-frosted-glass-941.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -141,19 +174,27 @@ XTroid.addCMD({pattern: '.n2bld ?(.*)', fromMe: wk, dontAddCMDList: true}, (asyn
                   });
               };
 
-              await download(`${data}`, '/root/lizy/n2bld.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/n2bld.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lblood.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lblood.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.ntoxic ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+
+XTroid.addCMD({pattern: 'ntoxic ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/toxic-text-effect-online-901.html",
+    lasiapi.textpro("https://textpro.me/toxic-text-effect-online-901.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -163,19 +204,26 @@ XTroid.addCMD({pattern: '.ntoxic ?(.*)', fromMe: wk, dontAddCMDList: true}, (asy
                   });
               };
 
-              await download(`${data}`, '/root/lizy/ntoxic.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/ntoxic.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/ltoxic.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/ltoxic.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.ncarbon ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+XTroid.addCMD({pattern: 'n2bld ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/carbon-text-effect-833.html",
+    lasiapi.textpro("https://textpro.me/horror-blood-text-effect-online-883.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -185,19 +233,26 @@ XTroid.addCMD({pattern: '.ncarbon ?(.*)', fromMe: wk, dontAddCMDList: true}, (as
                   });
               };
 
-              await download(`${data}`, '/root/lizy/ncarbon.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/ncarbon.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/l2blood.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/l2blood.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.nsteel ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+XTroid.addCMD({pattern: 'ncarb ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/eroded-metal-text-effect-834.html",
+    lasiapi.textpro("https://textpro.me/carbon-text-effect-833.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -207,19 +262,27 @@ XTroid.addCMD({pattern: '.nsteel ?(.*)', fromMe: wk, dontAddCMDList: true}, (asy
                   });
               };
 
-              await download(`${data}`, '/root/lizy/nsteel.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/nsteel.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lcb.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lcb.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.ncloud ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+
+XTroid.addCMD({pattern: 'nlux ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/create-a-cloud-text-effect-on-the-sky-online-1004.html",
+    lasiapi.textpro("https://textpro.me/3d-luxury-gold-text-effect-online-1003.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -229,19 +292,27 @@ XTroid.addCMD({pattern: '.ncloud ?(.*)', fromMe: wk, dontAddCMDList: true}, (asy
                   });
               };
 
-              await download(`${data}`, '/root/lizy/ncloud.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/ncloud.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lux.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lux.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
 
-XTroid.addCMD({pattern: '.nxmas ?(.*)', fromMe: wk, dontAddCMDList: true}, (async (message, match) => {
 
+
+
+XTroid.addCMD({pattern: 'ngold ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
+
+    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
+        const { lasi } = ann.data.def1
+        const lc = lasi
+        const seed = Config.LOCK
+        if (lc !== seed) return await message.sendMessage(GG);
     if (match[1] === '') return await message.sendMessage(need);
-    LASIapi.textpro("https://textpro.me/xmas-cards-3d-online-942.html",
+    lasiapi.textpro("https://textpro.me/hexa-golden-text-effect-842.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
@@ -251,11 +322,11 @@ XTroid.addCMD({pattern: '.nxmas ?(.*)', fromMe: wk, dontAddCMDList: true}, (asyn
                   });
               };
 
-              await download(`${data}`, '/root/lizy/nxmas.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/nxmas.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              await download(`${data}`, '/root/HTM/lgd.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/HTM/lgd.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
               })
           } catch(err) { 
               console.log(err)
           } 
-    });
+    });});
 }));
